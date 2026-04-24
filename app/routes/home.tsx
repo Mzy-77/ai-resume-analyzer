b157066407a91df29f3e3d2a66affa803e13b9a7
MZy-77 <mohamedezz22999@gmail.com>
2026-04-24 14:29:31 +0300

    navbar added

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,21 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Rsumuo" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('public/images/bg-main.svg')]">
+    <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Applications & Resume Rating</h1>
+          <h2>Drop your resume for an ATS score and improvement tips.</h2>
+        </div>
+      </section>
+  </main>
 }
