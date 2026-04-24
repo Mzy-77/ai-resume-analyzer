54dab5af0ce296babfccc3ca225049d07110149f
MZy-77 <mohamedezz22999@gmail.com>
2026-04-24 16:03:15 +0300

    card added

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,9 +1,11 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/navbar";
+import {resumes} from "~/constants";
+import Card from "~/components/Card";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Rsumuo" },
+    { title: " " },
     { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
@@ -17,5 +19,12 @@ export default function Home() {
           <h2>Drop your resume for an ATS score and improvement tips.</h2>
         </div>
       </section>
+    {resumes.length > 0 && (
+        <div className="resumes-section">
+              {resumes.map(resume => (
+                  <Card key={resume.id} resume={resume} />
+              ))}
+        </div>
+    )}
   </main>
 }
